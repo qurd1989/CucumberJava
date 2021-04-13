db662f0d1b67e48b8cb2e861c524a814649575db
Elmar Abdiyev <mashaedu@gmail.com>
2021-04-13 10:59:47 -0400

    new commnets added

--- src/test/java/seleniumSampleCodes/OpenIncognitoMode.java
+++ src/test/java/seleniumSampleCodes/OpenIncognitoMode.java
@@ -13,13 +13,22 @@ public class OpenIncognitoMode {
 
 	public static void main(String[] args) {
 	
+		 //setting up capabilities class 
 		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+		
+		// and then setting up chrome option class 
 		ChromeOptions options = new ChromeOptions();
+		
+		// setting browser property in incognito mode
 		options.addArguments("--incognito");
+		
+		//passing chrome option object into capabilites 
 		capabilities.setCapability(ChromeOptions.CAPABILITY, options);
 		
+		// webdriver setup 
 		WebDriverManager.chromedriver().setup();
 		
+		//launching the desired browser in incoginto mode
 		WebDriver driver =new ChromeDriver(capabilities);
 		driver.get("https://chercher.tech/java/incognito-browser-selenium");
 	    System.out.println(driver.getTitle());
