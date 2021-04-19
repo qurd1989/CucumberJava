875edf3d14e7d9f2d803d97d5d8e9e495020fa1e
Elmar Abdiyev <mashaedu@gmail.com>
2021-04-19 16:27:37 -0400

    new codes

--- src/test/java/seleniumSampleCodes/MultipleNumbersInGoogle.java
+++ src/test/java/seleniumSampleCodes/MultipleNumbersInGoogle.java
@@ -22,12 +22,16 @@ public class MultipleNumbersInGoogle {
 			
 		dr.get("https://www.google.com/");
 	
+		//open google and get search 
 		WebElement element = dr.findElement(By.name("q"));
 
+		//send 5*5*5 =125 on search
 		element.sendKeys(Keys.ENTER, "5*5*5",Keys.ENTER);
 	
 
+		//get XPath and use text() to get text from Xpath and used Descendant Technicque 
 		WebElement element2 =dr.findElement(By.xpath("//*[@class='z7BZJb XSNERd']//*[contains(@id,'cwos')]"));
+		//used is statment to validate the numbers
 		if(element2.getText().equals("125")) {
 			System.out.println("Number is Validated");
 		}else {
